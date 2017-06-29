858f30e3a2db60dd9825a38eac2f3db866e4ea74
Kevin Berg <kevin@saucelabs.com>
2017-06-29 09:17:10 -0700

    Add Framework

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# Java-TestNG-Appium-Android-Dynamic
+## Java-TestNG-Appium-Android-Dynamic
+
+>This code is presented as an example only, since your tests and testing environments may require specialized scripting. This information should be taken only as an
+>illustration of how one would set up tests with Sauce Labs, and any modifications will not be supported. For questions regarding Sauce Labs integration, please see 
+>our documentation at https://wiki.saucelabs.com/.
+
+### Environment Setup
+
+1. Global Dependencies
+    * Install Maven
+    	https://maven.apache.org/install.html
+    * Or Install Maven with Homebrew
+    	http://brew.sh/
+    ```
+    $ brew install maven
+    ```
+2. TestObject Credentials
+    * Add your TestObject API Key in line 52
+    ```
+    capabilities.setCapability("testobject_api_key", "API_KEY");
+    ```
+3. TestObject Credentials
+    * Ensure that your account has access to the devices below. If not, adjust accordingly
+    ```
+    new Object[]{"Android", "Samsung Galaxy S6", "6"},
+    new Object[]{"Android", "Google Pixel", "7"}
+    ```
+
+4. Project Dependencies
+    * Check that packages are available
+    ```
+    $ cd Java-TestNG-Appium-Android-Dynamic
+    $ mvn test-compile
+    ```
+    * You may also want to run the command below to check for outdated dependencies. Please be sure to verify and review updates before editing your pom.xml file as they may not be compatible with your code.
+    ```
+    $ mvn versions:display-dependency-updates
+    ```
+4. Application
+    * Application can be downloaded by clicking [here](https://github.com/testobject/calculator-test-gradle/blob/master/Calculator_2.0.apk)
+    
+### Running Tests
+```
+$ mvn test
+```
+
+### Advice/Troubleshooting
+1. It may be useful to use a Java IDE such as IntelliJ or Eclipse to help troubleshoot potential issues. 
+
+### Resources
+##### [TestObject REST API Documentation](https://api.testobject.com/)
+
+##### [Sauce Labs Documentation](https://wiki.saucelabs.com/)
+
+##### [TestNG Documentation](http://testng.org/doc/documentation-main.html)
+
+##### [Java Documentation](https://docs.oracle.com/javase/7/docs/api/)
+
+##### [Stack Overflow](http://stackoverflow.com/)
+* A great resource to search for issues not explicitly covered by documentation.
+
+
+
+
+
+
+
---
+++ src/test/java/com/saucelabs/example/TestSetup.java
@@ -0,0 +1,106 @@
+package com.saucelabs.example;
+
+import com.saucelabs.example.util.ResultReporter;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.openqa.selenium.OutputType;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by grago on 28/02/2017.
+ */
+public class TestSetup {
+
+    private ResultReporter reporter;
+    private ThreadLocal<AndroidDriver> driver = new ThreadLocal<AndroidDriver>();
+  
+  /**
+   * DataProvider that explicitly sets the browser combinations to be used.
+   *
+   * @param testMethod
+   * @return
+   */
+  @DataProvider(name = "devices", parallel = true)
+  public static Object[][] sauceBrowserDataProvider(Method testMethod) {
+      return new Object[][]{
+    		  //Verify that your account has access to the devices below
+              new Object[]{"Android", "Samsung Galaxy S6", "6"},
+              new Object[]{"Android", "Google Pixel", "7"}
+      };
+  }
+  
+  private AndroidDriver createDriver(String platformName, String platformVersion, String deviceName, String methodName) throws MalformedURLException {
+  	
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setCapability("testobject_api_key", "ADD_API_KEY_HERE");
+      capabilities.setCapability("deviceName", deviceName);
+      capabilities.setCapability("platformVersion", platformVersion);
+      capabilities.setCapability("platformName", platformName);
+      capabilities.setCapability("testobject_test_name",  methodName);
+      capabilities.setCapability("testobject_appium_version", "1.6.5");
+
+      driver.set(new AndroidDriver<WebElement>(
+              new URL("http://us1.appium.testobject.com/wd/hub"),
+              capabilities));
+      return driver.get();
+  }
+
+    /* A simple addition, it expects the correct result to appear in the result field. */
+    @Test(dataProvider = "devices")
+    public void twoPlusTwoOperation(String platformName, String deviceName, String platformVersion, Method method) throws MalformedURLException {
+
+    	AndroidDriver driver = createDriver(platformName, platformVersion, deviceName, method.getName());
+
+        /* Get the elements. */
+        MobileElement buttonTwo = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/digit2")));
+        MobileElement buttonPlus = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/plus")));
+        MobileElement buttonEquals = (MobileElement)(driver.findElement(By.id("net.ludeke.calculator:id/equal")));
+        MobileElement resultField = (MobileElement)(driver.findElement(By.xpath("//android.widget.EditText[1]")));
+
+        /* Add two and two. */
+        buttonTwo.click();
+        buttonPlus.click();
+        buttonTwo.click();
+        driver.getScreenshotAs(OutputType.FILE);
+        buttonEquals.click();
+        driver.getScreenshotAs(OutputType.FILE);
+
+        /* Check if within given time the correct result appears in the designated field. */
+        (new WebDriverWait(driver, 30)).until(ExpectedConditions.textToBePresentInElement(resultField, "4"));
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+    	AndroidDriver driver = getWebDriver();
+    	reporter = new ResultReporter();
+        boolean success = result.isSuccess();
+        String sessionId = driver.getSessionId().toString();
+
+        reporter.saveTestStatus(sessionId, success);
+        driver.quit();
+    }
+    
+    /**
+     * @return the {@link WebDriver} for the current thread
+     */
+    public AndroidDriver getWebDriver() {
+        return driver.get();
+    }
+}
